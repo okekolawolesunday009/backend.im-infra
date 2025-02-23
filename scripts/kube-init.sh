019df6ea41a9ba6988166d11b73180b45129fa45
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2025-02-23 13:49:03 +0100

    init kube v1

--- scripts/kube-init.sh
+++ scripts/kube-init.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 set -eo pipefail
 
-
-
-
 echo "Configuring K3s..."
+
+# Define K3S directory and file
 K3S_CONFIG_DIR="/etc/rancher/k3s"
 K3S_CONFIG="${K3S_CONFIG_DIR}/k3s.yaml"
-mkdir -p "${K3S_CONFIG_DIR}"
+
+# Create the directory with appropriate permissions
+if [ ! -d "$K3S_CONFIG_DIR" ]; then
+    echo "Creating $K3S_CONFIG_DIR"
+    sudo mkdir -p "${K3S_CONFIG_DIR}"
+    sudo chown -R backenduser:backenduser "${K3S_CONFIG_DIR}"
+fi
+
 sleep 10
 
 # Set up KUBECONFIG
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" | tee -a /etc/profile.d/k3s.sh
+echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" | sudo tee -a /etc/profile.d/k3s.sh
 
 # Ensure kubectl is accessible
 export PATH=$PATH:/usr/local/bin
